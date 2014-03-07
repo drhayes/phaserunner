e41bb415d4d0986768996210a97d8d144d969272
Ian Groff <ian@iangroff.com>
2014-03-06 21:35:09 -0600

    keep the platform moving if the player hits the edge

--- js/boxes.js
+++ js/boxes.js
@@ -5,16 +5,21 @@ module.exports = function(game) {
   var one = boxes.create(200, game.world.height - 164, 'box');
   one.scale.setTo(6, 1);
   one.body.immovable = true;
-  one.body.velocity.x = -60;
+  one.body.rebound = false;
+  var velocity = -50
   console.log(one);
   one.events.onOutOfBounds.add(function (face, thisBody, bodyThatHitMe){ 
-          one.body.velocity.x = - (one.body.velocity.x);
+          velocity = -velocity;
   });
+
+  one.body.collideCallback = function() { 
+    console.log("hit!");
+  }
 
   return {
     group: boxes,
     update: function() {
-
+      one.body.velocity.x = velocity;
     }
   };
 };
