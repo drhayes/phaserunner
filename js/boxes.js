d979f79e529fad45ab6b3b578a1b506f46e28283
Ian Groff <ian@iangroff.com>
2014-03-06 21:24:18 -0600

    platforms, moving, not awful

--- js/boxes.js
+++ js/boxes.js
@@ -2,26 +2,19 @@ var constants = require('./constants');
 
 module.exports = function(game) {
   var boxes = game.add.group();
-  boxes.createMultiple(60, 'box');
-  function addABox (x, y){
-    var box = boxes.getFirstDead();
-    box.body.velocity.x = -100;
-    box.body.immovable = true;
-    box.outOfBoundsKill = true;
-    box.reset(x, y);
-  };
-  function addBoxes() {  
-      var hole = Math.floor(Math.random()*5)+1;
-      for (var i = 0; i < 8; i++)
-          if (i != hole && i != hole +1) 
-              addABox(608, game.world.height - (i*32+10));   
-  };
-  var timer = game.time.events.loop(1500, addBoxes, this);
+  var one = boxes.create(200, game.world.height - 164, 'box');
+  one.scale.setTo(6, 1);
+  one.body.immovable = true;
+  one.body.velocity.x = -60;
+  console.log(one);
+  one.events.onOutOfBounds.add(function (face, thisBody, bodyThatHitMe){ 
+          one.body.velocity.x = - (one.body.velocity.x);
+  });
 
   return {
     group: boxes,
     update: function() {
-        boxes.setAll('body.velocity.x', -100);
+
     }
   };
 };
