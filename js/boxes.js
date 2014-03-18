22c7699363b5f65f14143069a7e0258a60913484
Ian Groff <ian@iangroff.com>
2014-03-17 21:28:03 -0500

    many boxes

--- js/boxes.js
+++ js/boxes.js
@@ -2,25 +2,33 @@ var constants = require('./constants');
 
 module.exports = function(game) {
   var boxes = game.add.group();
-  var one = boxes.create(200, game.world.height - 164, 'box');
-  game.physics.enable(one, Phaser.Physics.ARCADE);
-  one.body.immovable = true;
-  one.body.rebound = false;
-  var velocity = -50
-  one.checkWorldBounds = true;
-  one.events.onOutOfBounds.add(function (face, thisBody, bodyThatHitMe){
-    console.log("I'm out of bounds");
-    velocity = -velocity;
-  });
+  boxes.create(0, 0, 'box1', null, false);
+  boxes.create(0, 0, 'box2', null, false);
+  boxes.create(0, 0, 'box3', null, false);
+  boxes.create(0, 0, 'box4', null, false);
+  boxes.create(0, 0, 'box5', null, false);
+ 
 
-  one.body.collideCallback = function() {
-    console.log("hit!");
+  function addABox() {
+    var one = boxes.getFirstDead();
+    var width = one.key.replace(/box/, '') * 32;
+    game.physics.enable(one, Phaser.Physics.ARCADE);
+    one.exists = true;
+    one.body.immovable = true;
+    one.body.rebound = false;
+    one.checkWorldBounds = true;
+    one.outOfBoundsKill = true;
+    var height = ((Math.round(Math.random() * 6) + 2) * 32) + 1;
+    one.reset(game.world.width - width, game.world.height - height, 1000);
+    one.events.onOutOfBounds.add(function (){ addABox(); });
   }
+
+  addABox();
 
   return {
     group: boxes,
     update: function() {
-      one.body.velocity.x = velocity;
+      boxes.setAll('body.velocity.x', -100, true, false, 0);
     }
   };
 };
