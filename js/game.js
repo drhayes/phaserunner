14d45127f24a03eb909a281c6a90058c8c8697e7
David Hayes <drhayes@gmail.com>
2014-03-17 20:25:25 -0500

    The better ground.

--- js/game.js
+++ js/game.js
@@ -3,6 +3,7 @@ var constants = require('./constants');
 var playerChunks = require('./playerChunks');
 var player = require('./player');
 var boxes = require('./boxes');
+var ground = require('./ground');
 
 game = new Phaser.Game(640, 480, Phaser.AUTO, 'game_div');
 var main_state = {
@@ -16,28 +17,29 @@ var main_state = {
   },
   create: function() {
     this.sky = game.add.sprite(0, 0, 'sky');
-    this.ground = game.add.group();
-    this.ground.enableBody = true;
-    this.ground.physicsBodyType = Phaser.Physics.ARCADE;
-    this.ground.createMultiple(20, 'ground', 0, true);
-    var counter = 0;
-    this.ground.forEach(function(piece) {
-      piece.body.x = counter * 32;
-      piece.body.y = game.world.height - 32;
-      piece.body.immovable = true;
-      counter++;
-      console.log(piece);
-    })
+    // this.ground = game.add.group();
+    // this.ground.enableBody = true;
+    // this.ground.physicsBodyType = Phaser.Physics.ARCADE;
+    // this.ground.createMultiple(20, 'ground', 0, true);
+    // var counter = 0;
+    // this.ground.forEach(function(piece) {
+    //   piece.body.x = counter * 32;
+    //   piece.body.y = game.world.height - 32;
+    //   piece.body.immovable = true;
+    //   counter++;
+    //   console.log(piece);
+    // })
 
     this.boxes = boxes(game);
     this.playerChunks = playerChunks(game);
     this.player = player(game, this.playerChunks);
+    this.ground = ground(game);
   },
   update: function() {
-    game.physics.arcade.collide(this.player.sprite, this.ground);
+    game.physics.arcade.collide(this.player.sprite, this.ground.sprite);
     game.physics.arcade.collide(this.player.sprite, this.boxes.group);
     game.physics.arcade.collide(this.ground, this.boxes.group);
-    game.physics.arcade.collide(this.playerChunks.emitter, this.ground);
+    game.physics.arcade.collide(this.playerChunks.emitter, this.ground.sprite);
 
     this.player.update();
     this.boxes.update();
