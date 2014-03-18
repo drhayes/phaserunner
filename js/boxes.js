e094f74565802863b107bced40bec8c3bf2c47a0
Ian Groff <ian@iangroff.com>
2014-03-17 22:15:28 -0500

    renaming boxes

--- js/boxes.js
+++ js/boxes.js
@@ -2,25 +2,22 @@ var constants = require('./constants');
 
 module.exports = function(game) {
   var boxes = game.add.group();
-  boxes.create(0, 0, 'box1', null, false);
-  boxes.create(0, 0, 'box2', null, false);
-  boxes.create(0, 0, 'box3', null, false);
-  boxes.create(0, 0, 'box4', null, false);
-  boxes.create(0, 0, 'box5', null, false);
+  boxes.create(0, 0, 'box192', null, false);
+  boxes.create(0, 0, 'box160', null, false);
+  boxes.create(0, 0, 'box128', null, false);
+  boxes.create(0, 0, 'box96', null, false);
+  boxes.create(0, 0, 'box64', null, false);
  
 
   function addABox() {
     var one = boxes.getFirstDead();
-    var width = one.key.replace(/box/, '') * 32;
+    var width = one.key.replace(/box/, '');
     game.physics.enable(one, Phaser.Physics.ARCADE);
-    one.exists = true;
     one.body.immovable = true;
-    one.body.rebound = false;
     one.checkWorldBounds = true;
     one.outOfBoundsKill = true;
     var height = ((Math.round(Math.random() * 6) + 2) * 32) + 1;
     one.reset(game.world.width - width, game.world.height - height, 1000);
-    one.events.onOutOfBounds.add(function (){ addABox(); });
   }
 
   addABox();
